35cca76e8932534bc9535c1ae33b116c123a39a6
Hamed Bavafa <hamed.bavafa@valtech.com>
2023-03-09 13:31:07 +0100

    init

---
+++ apps/checkout/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ssr-sandbox-checkout-entry',
+  template: `<ssr-sandbox-nx-welcome></ssr-sandbox-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
---
+++ apps/store/src/remotes.d.ts
@@ -0,0 +1,3 @@
+declare module 'product/Module';
+
+declare module 'checkout/Module';
